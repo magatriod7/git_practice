ed0cc9d1325ff4398e750aa327c06e033cc20ed5
JH <magatriod7@naver.com>
2021-11-23 15:06:20 +0900

    console.log foreach arr plus

---
+++ main2.js
@@ -0,0 +1,23 @@
+ var cnt = 0;
+ var arr = [];
+	for(cnt = 0; cnt <100; cnt++){
+		arr.push(cnt+1);
+	}
+	
+
+	for(cnt = 0; cnt <100; cnt++){
+	
+		//if((cnt%3 === 0)&&(cnt % 5 === 0)){
+		//	console.log("fizzbuzz");
+		
+		//}
+		//else if(cnt%3 === 0){
+		//	console.log("fizz")
+		//}
+		//else if(cnt%5 === 0){
+		//	console.log("buzz")
+		//}
+		//else{
+			console.log(arr[cnt])
+		//}
+		}
